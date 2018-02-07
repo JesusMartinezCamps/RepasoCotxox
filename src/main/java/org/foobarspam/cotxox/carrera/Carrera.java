9cb34516b6d9327629e41d6d20f6b8fd2880a0d0
Jesús Martínez <gsus.martinez97@gmail.com>
2018-02-07 18:09:25 +0100

    Atributos, getters y setters de las clases Carrera y Tarifa añadidos

--- src/main/java/org/foobarspam/cotxox/carrera/Carrera.java
+++ src/main/java/org/foobarspam/cotxox/carrera/Carrera.java
@@ -1,5 +1,57 @@
 package org.foobarspam.cotxox.carrera;
 
-public class Carrera {
+import org.foobarspam.cotxox.conductores.*;
+import org.foobarspam.cotxox.tarifa.Tarifa;
 
+public class Carrera {
+	//Attributes
+	Conductor conductor = null;
+	
+	int tiempoEsperado = 0;
+	int tiempoCarrera= 0;
+
+	double costeTotal = 0.0;
+	double distancia = 0.0;
+	
+	String tarjetaCredito = "";
+	String origen = "";
+	String destino = "";
+	
+	
+	//Constructor
+	public Carrera(String tarjetaCredito) {
+		this.tarjetaCredito = tarjetaCredito;
+	}
+	
+	
+	//Getters and setters
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	public void setDestino(String destino) {
+		this.destino = destino;
+	}
+	public void setDistancia(double distancia) {
+		this.distancia = distancia;
+	}
+	public void setTiempoEsperado(int tiempoEsperado) {
+		this.tiempoEsperado = tiempoEsperado;
+	}
+	
+	
+	public String getTarjetaCredito() {
+		return this.tarjetaCredito;
+	}
+	public String getOrigen() {
+		return this.origen;
+	}
+	public String getDestino() {
+		return this.destino;
+	}
+	public double getDistancia() {
+		return this.distancia;
+	}
+	public double getCosteEsperado() {
+		return Tarifa.getCosteEsperado();
+	}
 }
---
+++ src/main/java/org/foobarspam/cotxox/tarifa/Tarifa.java
@@ -0,0 +1,11 @@
+package org.foobarspam.cotxox.tarifa;
+
+public class Tarifa {
+	//Attribute
+	int id = 0;
+	
+	//Methods
+	public static double getCosteEsperado() {
+		return 0;
+	}
+}
